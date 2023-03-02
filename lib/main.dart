f8a389b1ae9d74af95d5ae0ca3a3137b66d60451
nehal Alhablani <nehal.alanazi@gmail.com>
2023-03-02 03:00:14 +0300

    add image in the button and changes style

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +16,7 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("project Tracker"),
           centerTitle: true,
-          backgroundColor: Colors.transparent,
+          backgroundColor: Color.fromARGB(221, 2, 97, 45),
         ),
         body: Center(
           child: Column(
@@ -28,8 +30,11 @@ class MainApp extends StatelessWidget {
                 ),
               ),
               Container(
+                margin: EdgeInsets.all(15),
+              ),
+              Container(
                 height: 30,
-                width: 200,
+                width: 230,
                 child: TextField(
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(hintText: 'User Name'),
@@ -39,53 +44,76 @@ class MainApp extends StatelessWidget {
               Container(
                 margin: EdgeInsets.all(10),
                 height: 30,
-                width: 200,
+                width: 230,
                 child: TextField(
                   textAlign: TextAlign.center,
-                  decoration: InputDecoration(hintText: 'Password'),
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                  ),
                 ),
                 color: Colors.white,
               ),
               ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                  onPressed: () => {print("loge in")},
-                  child: Text("Loge In")),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 2, 80, 5)),
+                  onPressed: () => {print("log in")},
+                  child: Text("   Log In   ")),
               Container(
-                margin: EdgeInsets.all(10),
+                margin: EdgeInsets.all(2),
               ),
               ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 2, 80, 5)),
                   onPressed: () => {print("Sign in")},
-                  child: Text("Sign in")),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('----'),
-                  Text('Continue with'),
-                  Text('----'),
-                ],
+                  child: Text("   sign in  ")),
+              Container(
+                margin: EdgeInsets.all(9),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Google'),
-                  Text('Apple'),
-                  Text('FaceBook'),
+                  Text('----'),
+                  Text(' or ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          color: Color.fromARGB(237, 11, 6, 6))),
+                  Text('----'),
                 ],
+              ),
+              Container(
+                margin: EdgeInsets.all(8),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  onPressed: () => {print("Sign in")},
+                  child: Container(
+                      width: 200,
+                      height: 20,
+                      child: Image.asset('images/GoogleLogo.svg.png')),
+                )
+              ]),
+              Container(
+                margin: EdgeInsets.all(8),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text('do you have an account ?'),
-                  Text('Create new account'),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('Create new account'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.blue,
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
-        backgroundColor: Color.fromARGB(255, 202, 140, 170),
+        backgroundColor: Color.fromARGB(255, 241, 244, 243),
       ),
     );
   }
