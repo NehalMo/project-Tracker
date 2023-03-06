24594369821da45cfdb5ff2e9a8d1ab5d1f10927
nehal Alhablani <nehal.alanazi@gmail.com>
2023-03-06 17:07:50 +0300

    change drawer color

--- lib/myDrawer.dart
+++ lib/myDrawer.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class MyDrawer extends StatelessWidget {
   const MyDrawer({
     super.key,
@@ -10,45 +8,42 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      
-      child: Column(children: [ 
-
-UserAccountsDrawerHeader(
-  currentAccountPicture: CircleAvatar(backgroundColor: Colors.green,child: Text("a"),) ,
- accountName: Text("Aisha"),
-  accountEmail: Text("aa@gmail.com"),
-  ),
-
-
-          ListTile(
-            onTap: () {
-              print("did tap Profile");
-            },
-            leading: Icon(Icons.person),
-            title: Text("Profile"),
-            subtitle: Text("this is your Profile page"),
+        child: Column(
+      children: [
+        UserAccountsDrawerHeader(
+          currentAccountPicture: CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 24, 103, 241),
+            child: Text("a"),
           ),
-          
-          ListTile(
-            onTap: () {
-              print("did tap Home");
-            },
-            leading: Icon(Icons.home),
-            title: Text("Home"),
-            subtitle: Text("this is the Home page"),
-          ),
-          
-          ListTile(
-            onTap: () {
-              print("did tap Search");
-            },
-            leading: Icon(Icons.logout),
-            title: Text("Log out"),
-            subtitle: Text("this is the Log out"),
-           
-          ),
-        ],
-      
+          accountName: Text("Aisha"),
+          accountEmail: Text("aa@gmail.com"),
+          decoration: BoxDecoration(color: Color.fromARGB(255, 168, 243, 205)),
+        ),
+        ListTile(
+          onTap: () {
+            print("did tap Profile");
+          },
+          leading: Icon(Icons.person),
+          title: Text("Profile"),
+          subtitle: Text("this is your Profile page"),
+        ),
+        ListTile(
+          onTap: () {
+            print("did tap Home");
+          },
+          leading: Icon(Icons.home),
+          title: Text("Home"),
+          subtitle: Text("this is the Home page"),
+        ),
+        ListTile(
+          onTap: () {
+            print("did tap Search");
+          },
+          leading: Icon(Icons.logout),
+          title: Text("Log out"),
+          subtitle: Text("this is the Log out"),
+        ),
+      ],
     ));
   }
 }
